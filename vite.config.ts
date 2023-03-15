4f0188f3d3ff75564a9fe918a2cf17bc66ecde6f
Vadim Zaboturin <vadimzab@gmail.com>
2023-03-15 22:37:39 +0700

    fix build options

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,46 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import svgLoader from 'vite-svg-loader'
 
+const appVersion = process.env.npm_package_version.replace(/\./g, "")
+const htmlPlugin = () => {
+    return {
+        name: 'html-transform',
+        transformIndexHtml(html) {
+            return html
+                .replaceAll('.js', '.js?v=' + appVersion)
+                .replaceAll('.css', '.css?v=' + appVersion)
+        },
+    }
+}
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-      vue(),
-      svgLoader()
-  ],
-  server: {
+    plugins: [
+        vue(),
+        svgLoader(),
+        htmlPlugin(),
+    ],
+    build: {
+        rollupOptions: {
+            output: {
+                chunkFileNames: `assets/js/[name].[hash].js`,
+                entryFileNames: `assets/js/[name].[hash].js`,
+                assetFileNames: (assetInfo) => {
+                    let extType = assetInfo.name.split('.').pop();
+                    if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+                        extType = 'img';
+                    }
+                    if (/eot|ttf|woff2?/i.test(extType)) {
+                        extType = 'font';
+                    }
+                    return `assets/${extType}/[name].[hash][extname]`;
+                },
+            }
+        }
+    },
+    server: {
     host: '0.0.0.0'
-  },
-  define: {
-    APP_VERSION: JSON.stringify(process.env.npm_package_version),
-}
+    },
+    define: {
+        APP_VERSION: JSON.stringify(process.env.npm_package_version),
+    }
 })
