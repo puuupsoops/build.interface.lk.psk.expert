6437aeb03b6f4992845a41d39d3ab1c0c7af46bf
Vadim Zaboturin <vadimzab@gmail.com>
2023-05-10 23:46:26 +0700

    ORG-40

--- src/models/Orders.ts
+++ src/models/Orders.ts
@@ -12,18 +12,24 @@ export interface OrdersResponse{
 
 
 export interface Orders {
-    id:           string; // !deprecated <string> временно оставлено пустым 
-    name:         string; // имя заказа
-    partner_name: string; // имя контрагента
-    partner_guid: string; // идентификатор контрагента
-    user_id:      number; // дентификатор пользователя в битрикс
-    date:         string; // дата создания заказа
-    status:       string; // статус заказа текстом
-    status_code:  number; // код статуса заказа
-    n:            number; // идентификатор общего заказа в таблице заказов Битрикс
-    checks:       Check[] | null; //массив со счетами 
-    reserved:     Boolean; // Признак того что заказ в резерве
-    comment:      String;
+    id:                  string; // !deprecated <string> временно оставлено пустым
+    name:                string; // имя заказа
+    partner_name:        string; // имя контрагента
+    partner_guid:        string; // идентификатор контрагента
+    user_id:             number; // дентификатор пользователя в битрикс
+    date:                string; // дата создания заказа
+    status:              string; // статус заказа текстом
+    status_code:         number; // код статуса заказа
+    n:                   number; // идентификатор общего заказа в таблице заказов Битрикс
+    checks:              Check[] | null; //массив со счетами
+    reserved:            Boolean; // Признак того что заказ в резерве
+    comment:             String;
+    shipment_name:       string|null, // До транспортной, Доставка, Самовывоз
+    shipment_id:         number|null,
+    delivery_terms:      string|null, // Другая, ПЭК, Байкал (НЕ ИСПОЛЬЗУЕТСЯ), Деловые линии, ЦЭП
+    delivery_terms_id:   number|null,
+    delivery_terms_code: string|null,
+    upd?:                string
 }
 
 export interface Check {
