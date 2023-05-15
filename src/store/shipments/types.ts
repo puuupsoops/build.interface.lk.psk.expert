0fc4e8243dc473edb3ba48bbc73d545282eaec4b
1cweb <1cweb@sq2.lan.psk.expert>
2023-05-15 10:35:06 +0300

    Добавлена компания ЦАП в список отгрузок

--- src/store/shipments/types.ts
+++ src/store/shipments/types.ts
@@ -14,17 +14,19 @@ export interface ShipmentsState {
 export const DeliveryName = <SelectInputData[]>[
     {id: 0, name: 'САМОВЫВОЗ'},
     {id: 1, name: 'ДЕЛОВЫЕ ЛИНИИ'}, 
-    {id: 2, name: 'БАЙКАЛ'},
+    //{id: 2, name: 'БАЙКАЛ'},
     {id: 3, name: 'ПЭК'},
     {id: 4, name: 'ДРУГАЯ ТРАНСПОРТНАЯ'},
+    {id: 5, name: 'ЦАП'},
 ]
 
 export const DeliveryCode =  [
     {id: 0, code: 'pickup'},
     {id: 1, code: 'del_line'}, 
-    {id: 2, code: 'baikal'},
+    //{id: 2, code: 'baikal'},
     {id: 3, code: 'pek'},
     {id: 4, code: 'other'},
+    {id: 5, name: 'cap'}
 ]
 
 export const PSKWorkTime: string = '8.00 до 18.00 (пн-пт) сб,вс-вых.'
